7fb2abe03b53e443545c4151acde0de099d60bfe
Louisemrk <louise.maarek@student-cs.fr>
2018-11-15 11:45:11 +0100

    nettoyage

--- Twitterpredictor/twitter_collect/twitterconnect.py
+++ Twitterpredictor/twitter_collect/twitterconnect.py
@@ -19,3 +19,9 @@ def twitter_setup():
 
 
 
+
+
+
+
+
+
--- untitled/venv/new/scratch_22.py
+++
@@ -1,8 +0,0 @@
-
-import tweepy
-
-from credentials import *
-
-for status in tweepy.Cursor(api.home_timeline).items(10):
-    # Process a single status
-    print(status.text)
