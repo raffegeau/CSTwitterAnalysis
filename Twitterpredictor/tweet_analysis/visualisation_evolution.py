e4d93022c3cf288b9eb8a857f28656a815e9ce24
CHarles Raffegeau <charles.raffegeau@student#hyhgit remote set-url origin git@github.com:raffegeau/CSTwitterAnalysis.gitgit config --global user.email charles.raffegeau@student>
2018-11-16 10:51:36 +0100

    analyse graphique de data

---
+++ Twitterpredictor/tweet_analysis/visualisation_evolution.py
@@ -0,0 +1,18 @@
+import tweet_collection.generer_donnees as tw
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#data=tw.create_data()
+
+def afficher_RT_Likes():
+    tfav = pd.Series(data=data["retweet_count"].values, index=data['created_at'])
+    tret = pd.Series(data=data["favorite_count"].values, index=data['created_at'])
+
+    # Likes vs retweets visualization:
+    tfav.plot(figsize=(16,4), label="retweet_count", legend=True)
+    tret.plot(figsize=(16,4), label="favorite_count", legend=True)
+
+    plt.show()
+
+
+#afficher_like_par_candidat
