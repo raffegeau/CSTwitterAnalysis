caeb9b7837926658d79e498adf0681741586f642
CHarles Raffegeau <charles.raffegeau@student#hyhgit remote set-url origin git@github.com:raffegeau/CSTwitterAnalysis.gitgit config --global user.email charles.raffegeau@student>
2018-11-16 14:47:32 +0100

    ajout fonction analyse graphique

--- Twitterpredictor/tweet_analysis/visualisation_evolution.py
+++ Twitterpredictor/tweet_analysis/visualisation_evolution.py
@@ -2,15 +2,15 @@ import tweet_collection.generer_donnees as tw
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#data=tw.create_data()
+data=tw.create_data()
 
 def afficher_RT_Likes(data):
-    tfav = pd.Series(data=data["retweet_count"].values, index=data['created_at'])
-    tret = pd.Series(data=data["favorite_count"].values, index=data['created_at'])
+    tret = pd.Series(data=data["retweet_count"].values, index=data['created_at'])
+    tfav = pd.Series(data=data["favorite_count"].values, index=data['created_at'])
 
     # Likes vs retweets visualization:
-    tfav.plot(figsize=(16,4), label="retweet_count", legend=True)
-    tret.plot(figsize=(16,4), label="favorite_count", legend=True)
+    tret.plot(figsize=(16,4), label="retweet_count", legend=True)
+    tfav.plot(figsize=(16,4), label="favorite_count", legend=True)
 
     plt.show()
 
@@ -18,9 +18,24 @@ def afficher_RT_Likes(data):
 #afficher_like_par_candidat
 
 def afficher_like_candidat(liste_candidat,data):
+    data.add(data["candidat"],axis="rows")
     for candidat in liste_candidat:
-        data.add(data[candidat],axis="rows")
-        for
+        l=len(data["texte"])
+        for i in range(l):
+            if candidat in data["texte"][i]:
+                data["candidat"][i]=candidat
+    tret = pd.Series(data=data["favorite_count"].values, index=data["candidate"])
+    tret.plot(figsize=(16,4), label="retweet_count", legend=True)
+
+print(afficher_like_candidat(["@EmmanuelMacron","@MarineLepen"],data))
+
+
+
+
+
+
+
+
 
 
 
