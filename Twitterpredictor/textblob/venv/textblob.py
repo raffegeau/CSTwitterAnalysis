0b081de4ff87a1c409977ad8f0ba32ed609698bd
Louisemrk <louise.maarek@student-cs.fr>
2018-11-16 15:55:43 +0100

    fonctionnalité 9

--- Twitterpredictor/textblob/venv/textblob.py
+++ Twitterpredictor/textblob/venv/textblob.py
@@ -18,5 +18,21 @@ def voctweet(data):
         if words[word]==1 or word.lemmatize()==word:
             uniquesoulem.append(word)
 
+def opiniontweet(data):
+    pos_tweets=[]
+    neu_tweets=[]
+    neg_tweets=[]
+    for tweet in data['tweet_textual_content'] :
+        txt=TextBlob(tweet)
+        n=tweet.sentiment[0]
+        if n<-0.5:
+            neu_tweets.append(txt)
+        elif n>0.5:
+            pos_tweets.append(txt)
+        else:
+            neg_tweets.append(txt)
 
+    print("Percentage of positive tweets: {}%".format(len(pos_tweets)*100/len(data['tweet_textual_content'])))
+    print("Percentage of neutral tweets: {}%".format(len(neu_tweets)*100/len(data['tweet_textual_content'])))
+    print("Percentage de negative tweets: {}%".format(len(neg_tweets)*100/len(data['tweet_textual_content'])))
 
